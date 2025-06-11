afc0406b5fdeb3be7d193c54918c99671ebebffb
ericstober <erictstober@gmail.com>
2025-06-10 22:18:44 -0500

    Add close ticket button

--- src/app/tickets/[id]/page.tsx
+++ src/app/tickets/[id]/page.tsx
@@ -3,6 +3,7 @@ import { logEvent } from "@/utils/sentry";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { getPriorityClass } from "@/utils/ui";
+import CloseTicketButton from "@/components/CloseTicketButton";
 
 const TicketDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
   const { id } = await props.params;
@@ -40,6 +41,8 @@ const TicketDetailsPage = async (props: { params: Promise<{ id: string }> }) => 
         >
           Back to Tickets
         </Link>
+
+        {ticket.status !== "Closed" && <CloseTicketButton ticketId={ticket.id} isClosed={ticket.status === "Closed"} />}
       </div>
     </div>
   );
