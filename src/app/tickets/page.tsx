2d4c29fcbf008e338837ab00662162e3bc6d3b1f
ericstober <erictstober@gmail.com>
2025-06-10 21:49:47 -0500

    get user and if not user redirect to login

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,8 +1,16 @@
 import { getTickets } from "@/actions/ticket.actions";
 import Link from "next/link";
 import { getPriorityClass } from "@/utils/ui";
+import { getCurrentUser } from "@/lib/current-user";
+import { redirect } from "next/navigation";
 
 const TicketsPage = async () => {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
   const tickets = await getTickets();
 
   return (
